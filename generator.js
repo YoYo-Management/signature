e126af4dfce56c1a16e02c8c3de9a0dfc67300ef
frankieeedeee <42694851+frankieeedeee@users.noreply.github.com>
2021-06-19 21:09:09 +1000

    Init Commit

---
+++ generator.js
@@ -0,0 +1,87 @@
+const MINIFIED_DATA = `<table border="0" cellpadding="0" cellspacing="0" style="line-height: 1rem;"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td style="line-height: 1rem;"> <table border="0" cellpadding="0" cellspacing="0" class="yoyomusic-signature" style="width: 420px; max-width: 100%; box-sizing: border-box; line-height: 1rem; border-collapse: collapse;" width="420"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td class="header-banner-wrap" style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif; width: 320px;" width="320"> <table border="0" cellpadding="0" cellspacing="0" class="header-banner" style="line-height: 1rem; border-collapse: collapse; background-color: rgb(0, 0, 0); border-radius: 3px 12px 3px 12px; width: 100%;" width="100%" bgcolor="rgb(0, 0, 0)"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td class="header-banner-container" style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif; padding: 10px 32px 10px 8px;"> <table border="0" cellpadding="0" cellspacing="0" style="line-height: 1rem; border-collapse: collapse;"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif;"> <img src="https://yoyomusic-public.s3-ap-southeast-2.amazonaws.com/yoyomusic/yoyomusic-logo-email.png" alt="YoYo Management Logo" width="54" height="54" style="line-height: 1rem;"> </td><td class="name-title" style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif; padding-left: 18px;"> <table border="0" cellpadding="0" cellspacing="0" style="line-height: 1rem; border-collapse: collapse;"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td id="signature-name" class="heading-name" style="font-family: 'Helvetica Neue', sans-serif; color: #fff; font-size: 18px; line-height: 19px; font-weight: 600; padding-top: 0; padding-bottom: 0;">{{name}}</td></tr><tr style="line-height: 1rem;"> <td id="signature-title" class="heading-title" style="font-family: 'Helvetica Neue', sans-serif; font-size: 13px; line-height: 17px; font-weight: 400; color: rgb(230, 230, 230); padding-top: 0; padding-bottom: 0;">{{title}}</td></tr></tbody> </table> </td></tr></tbody> </table> </td></tr></tbody> </table> </td><td style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif;"></td></tr><tr style="line-height: 1rem;"> <td style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif;"> <table border="0" cellpadding="0" cellspacing="0" class="contact-container" style="line-height: 1rem; border-collapse: collapse; margin-top: 12px;"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td class="contact-pipe-art-container" style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif; height: 100%; padding-left: 8px; padding-right: 12px; vertical-align: top;" height="100%" valign="top"> <div class="contact-pipe-art" style="line-height: 1rem; width: 8px; height: 100%; min-height: 64px; background: rgb(166,17,208); background: linear-gradient(140deg, rgba(166,17,208,1) 0%, rgba(58,67,255,1) 100%); border-radius: 1px 5px 1px 5px; white-space: pre; overflow: hidden;"> </div></td><td style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif;"> <table border="0" cellpadding="0" cellspacing="0" style="line-height: 1rem; border-collapse: collapse;"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td class="contact" style="font-family: 'Helvetica Neue', sans-serif; font-size: 13px; line-height: 16px; font-weight: 300; color: rgb(44, 44, 44); padding-top: 0; padding-bottom: 4px;"><b style="line-height: 1rem; font-weight: 800;">E:</b> <a id="signature-email" href="mailto:{{email}}" style="line-height: 1rem; color: inherit;">{{email}}</a></td></tr><tr style="line-height: 1rem;"> <td class="contact" style="font-family: 'Helvetica Neue', sans-serif; font-size: 13px; line-height: 16px; font-weight: 300; color: rgb(44, 44, 44); padding-top: 0; padding-bottom: 4px;"><b style="line-height: 1rem; font-weight: 800;">P:</b> <a href="tel:+61390708874" style="line-height: 1rem; color: inherit;">+61 3 9070 8874</a> <span class="contact-divider" style="line-height: 1rem; margin-left: 6px; margin-right: 6px;">|</span> <b style="line-height: 1rem; font-weight: 800;">M:</b> <a id="signature-mobile" href="tel:{{mobile_e164}}" style="line-height: 1rem; color: inherit;">{{mobile}}</a></td></tr><tr style="line-height: 1rem;"> <td class="contact contact-address" style="font-family: 'Helvetica Neue', sans-serif; font-size: 13px; line-height: 16px; font-weight: 300; color: rgb(44, 44, 44); padding-top: 4px; padding-bottom: 8px;">PO Box 173, Eltham VIC 3095, Australia</td></tr></tbody> </table> </td></tr></tbody> </table> </td></tr></tbody> </table> </td></tr><tr style="line-height: 1rem;"> <td style="line-height: 1rem;"> <table class="yoyomusic-signature signature-footer" style="max-width: 100%; box-sizing: border-box; line-height: 1rem; border-collapse: collapse; width: 100%;" width="100%"> <tbody style="line-height: 1rem;"> <tr style="line-height: 1rem;"> <td style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif;"> <hr class="hr" style="line-height: 1rem; border: none; margin-left: 0; width: 100%; max-width: 560px; height: 1px; margin-top: 4px; margin-bottom: 4px; background: rgb(235,235,235); background: linear-gradient(90deg, rgba(235,235,235,1) 50%, rgba(235,235,235,0) 90%); white-space: pre; overflow: hidden;"> </td></tr><tr style="line-height: 1rem;"> <td class="footer-text" style="font-family: 'Helvetica Neue', sans-serif; font-size: 12px; line-height: 14px; font-weight: 300; color: rgb(66, 66, 66); padding-top: 0; padding-bottom: 4px;">YoYo Management acknowledges the <a href="https://www.wurundjeri.com.au/" target="_blank" style="line-height: 1rem; color: inherit;">Wurundjeri People</a> as the traditional owners of this land upon which we live, learn and work. We pay respect to Elders past, present and emerging.</td></tr><tr style="line-height: 1rem;"> <td style="line-height: 1rem; font-family: 'Helvetica Neue', sans-serif;"> <br style="line-height: 1rem;"> </td></tr></tbody> </table> </td></tr></tbody> </table>`;
+
+$(document).ready(function () {
+    $("#generator-form").on("submit", function (e) {
+        e.preventDefault();
+        let name = $("#full-name").val();
+        let title = $("#title").val();
+        let email = $("#email").val();
+        let mobile = $("#mobile").val();
+
+        // Parse Mobile
+        let mobileParsed = libphonenumber.parsePhoneNumberFromString(mobile, "AU");
+        if (typeof mobileParsed === "undefined")
+        {
+            return alert("Mobile is not a valid Australian Number");
+        }
+
+        let data = MINIFIED_DATA;
+
+        data = data.replace('{{name}}', name)
+            .replace('{{title}}', title)
+            .replace('{{email}}', email)
+            .replace('{{email}}', email)
+            .replace('{{mobile}}', mobileParsed.formatInternational())
+            .replace('{{mobile_e164}}', mobileParsed.number);
+
+        // Fill Preview
+        $("#preview").html(data);
+
+        // Fill Raw Code Field
+        $("#raw-code-copy").val(data);
+    });
+
+    // Copy Request
+    let copy = new ClipboardJS("#copy");
+
+    copy.on('success', function(e) {
+        alert("Copied! Now, just paste it into your email client");
+        e.clearSelection();
+    });
+
+    // Copy Raw Code
+    let copyRaw = new ClipboardJS("#copy-raw");
+
+    copyRaw.on('success', function (e) {
+        alert("Copied! Now, you'll need to locate the relevant signature file and replace its contents with what you have just copied. Follow the instructions link to find out more");
+        e.clearSelection();
+    });
+
+    // Determine if there is any prefill info
+    const params = new URLSearchParams(window.location.search);
+    let prefil = false;
+
+    // Name
+    if (params.has('name'))
+    {
+        prefil = true;
+        $("#full-name").val(params.get('name'));
+    }
+    
+    // Title
+    if (params.has('title'))
+    {
+        prefil = true;
+        $("#title").val(params.get('title'));
+    }
+    
+    // Email
+    if (params.has('email'))
+    {
+        prefil = true;
+        $("#email").val(params.get('email'));
+    }
+    
+    // Mobile
+    if (params.has('mobile'))
+    {
+        prefil = true;
+        $("#mobile").val(params.get('mobile'));
+    }
+
+    if (prefil)
+    {
+        // Trigger signature generation
+        $("#generator-form").trigger('submit');
+    }
+});
